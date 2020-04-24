0ef3083409881359387dc18eb54b29dcf84bd160
David <doliverosescribano82@gmail.com>
2020-04-24 14:50:41 +0200

    no message

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,3 @@
+self.addEventListener("fetch", function(event){
 
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
-
-if (workbox) {
-  console.log('Yay! Workbox is loaded');
-} else {
-  console.log('Boo! Workbox didn');
-}
-
-import {registerRoute} from 'workbox-routing';
-import {NetworkFirst} from 'workbox-strategies';
-
-registerRoute(
-  /\.js$/,
-  new NetworkFirst()
-);
+});
