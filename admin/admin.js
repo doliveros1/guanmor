9b944d04c02f345d0ad45a06c0efce7f744c017b
David <doliverosescribano82@gmail.com>
2020-05-04 17:30:34 +0200

    no message

--- admin/admin.js
+++ admin/admin.js
@@ -1,5 +1,7 @@
 var API_PATH_ADMIN = "https://guanmor.herokuapp.com/api/guanmor/1.0.0";
-var indexCategory = 10;
+var indexCategory = 0;
+var indexProduct = 0;
+
 const LOGIN = "./login.html";
 
 var jwtToken;
@@ -7,6 +9,7 @@ var localId;
 var MENU_INFO = {};
 var LOCAL_INFO = {};
 var MAP_CATEGORIES_ID = new Map();
+var MAP_PRODUCTS_ID = new Map();
 var nav;
 
 window.onload = (e) => { 
@@ -26,6 +29,9 @@ addNewCategory = function (id) {
     indexCategory = indexCategory + 1;
     var category = "category"+indexCategory;
 	var value = document.getElementById(id).value;
+	if(value === ""){
+		presentToast("El nombre de la categoría no puede estar vacía");
+	} else {
 	document.getElementById("newCategoryId").value="";
 
 	var lastNode = document.getElementById("newCategoryItem");
@@ -52,16 +58,57 @@ addNewCategory = function (id) {
         		</ion-buttons>        		
           		<ion-input value="`+value+`" class="ion-text-wrap">
           		</ion-input>`;
-    document.getElementById("listCategories").insertBefore(elem,lastNode);
+    document.getElementById("listCategories").insertBefore(elem,lastNode);		
+	}
+};
+
+addNewProduct= function (idCategory, idProduct) {
+    indexProduct = indexProduct + 1;
+    var product = "product"+indexProduct;
+	var value = document.getElementById(idProduct).value;
+	if(value === ""){
+		presentToast("El nombre del producto no puede estar vacío");
+	}else {
+	document.getElementById("newProductId").value="";
+
+	var lastNode = document.getElementById("newProductItem");
+	
+	var elem = document.createElement('ion-item');
+	elem.setAttribute("id", product);
+
+	var productObject = {};
+	productObject.title = value;
+	productObject.products = [];
+	MAP_CATEGORIES_ID.get(idCategory).products.push(productObject);
+	MAP_PRODUCTS_ID.set(product, productObject);
+
+    elem.innerHTML = `<ion-buttons slot="end">
+        			<ion-button color="vibrant" onclick="removeProduct('`+idCategory+`','`+product+`')">
+      					<ion-icon slot="icon-only" name="trash-outline"></ion-icon>
+        			</ion-button>
+        			<ion-button color="vibrant">        		
+        				<ion-icon slot="icon-only"  name="heart-outline"></ion-icon>
+  					</ion-button>
+        		</ion-buttons>        		
+          		<ion-input value="`+value+`" class="ion-text-wrap">
+          		</ion-input>`;
+	document.getElementById("listProducts").insertBefore(elem,lastNode);
+	}
 
 };
 
 removeCategory = function (id) {
-	var lastNode = document.getElementById(id).remove();
+	document.getElementById(id).remove();
 	MAP_CATEGORIES_ID.delete(id);
 };  
-
-//
+removeProduct = function (idCategory, idProduct) {
+	document.getElementById(idProduct).remove();
+	var products = MAP_CATEGORIES_ID.get(idCategory).products;
+	var product = MAP_PRODUCTS_ID.get(idProduct);
+	products = products.filter(item => item.title !== product.title);
+	MAP_CATEGORIES_ID.get(idCategory).products = products;
+	console.info(products);
+};  
 
 selectConfiguration = function (idConfiguration) {
 	if(idConfiguration === "local"){
@@ -79,7 +126,7 @@ selectConfiguration = function (idConfiguration) {
 		document.getElementById("local").style.display="none";
 		document.getElementById("carta").style.display="none";
 		document.getElementById("code").style.display="block";		
-		document.getElementById("mainTitle").innerHTML="Generar código";			
+		document.getElementById("mainTitle").innerHTML="Comparte tu carta";			
 	} else if(idConfiguration === "exit"){
 		goToLogin();		
 	}
@@ -114,77 +161,114 @@ function setLocalInfo(localInfo){
 
 function setMenuInfo(menuInfo){
 	MAP_CATEGORIES_ID = new Map();
+	var categoryHTML = "";
 	MENU_INFO = menuInfo;
-	customElements.define('nav-categories', class NavHome extends HTMLElement {
-		connectedCallback() {
-		  var categoryHTML = "";
-
-		  categoryHTML = `
-			<ion-header translucent>
-			  <ion-toolbar>
-				<ion-title>Categorías</ion-title>
-			  </ion-toolbar>
-			</ion-header>
-			<ion-content fullscreen>
-			  <ion-list id="listCategories">`;
+	var listCategories = document.getElementById("listCategories");
 			  
-			  menuInfo[0].categories.forEach(category=>{
-				indexCategory = indexCategory +1;
-				var idCategory = "category"+indexCategory;
-				MAP_CATEGORIES_ID.set(idCategory,category);
-				categoryHTML = categoryHTML + `<ion-item id="`+idCategory+`">
-				<ion-buttons slot="end">
-				  <ion-button color="vibrant" onclick="removeCategory('`+idCategory+`')" >
-						<ion-icon slot="icon-only" name="trash-outline"></ion-icon>
-				  </ion-button>
-				  <ion-button color="vibrant">        		
-					  <ion-icon slot="icon-only"  name="heart-outline"></ion-icon>
-					</ion-button>
-					<ion-button color="vibrant" onclick="showCategoryDetail('`+idCategory+`')">        		
-					  <ion-icon slot="icon-only"  name="pencil-outline"></ion-icon>
-					</ion-button>
-			  </ion-buttons>
-		
-				<ion-input value="`+category.title+`" class="ion-text-wrap">
-				</ion-input>
-		  </ion-item>`;
-			});
-			categoryHTML = categoryHTML + `<ion-item id="newCategoryItem">
-			<ion-button color="vibrant" onclick="addNewCategory('newCategoryId')" slot="end">
-				Añadir
-			  </ion-button>          		
-				<ion-input id="newCategoryId" value="" placeholder="Escriba nombre de la categoría" class="ion-text-wrap">
-				</ion-input>
-			</ion-item>`;
-			categoryHTML = categoryHTML+  `</ion-list>
-			</ion-content>
-		  `;
-		  this.innerHTML = categoryHTML;
-		  console.info(categoryHTML);
-		}
-	  });
-	
+	menuInfo[0].categories.forEach(category=>{
+		indexCategory = indexCategory +1;
+		var idCategory = "category"+indexCategory;
+		MAP_CATEGORIES_ID.set(idCategory,category);
+		categoryHTML = categoryHTML + `<ion-item id="`+idCategory+`">
+		<ion-buttons slot="end">
+		  <ion-button color="vibrant" onclick="removeCategory('`+idCategory+`')" >
+				<ion-icon slot="icon-only" name="trash-outline"></ion-icon>
+		  </ion-button>
+		  <ion-button color="vibrant">        		
+			  <ion-icon slot="icon-only"  name="heart-outline"></ion-icon>
+			</ion-button>
+			<ion-button color="vibrant" onclick="showCategoryDetail('`+idCategory+`')">        		
+			  <ion-icon slot="icon-only"  name="pencil-outline"></ion-icon>
+			</ion-button>
+	  </ion-buttons>
+
+		<ion-input value="`+category.title+`" class="ion-text-wrap">
+		</ion-input>
+  </ion-item>`;
+	});
+	categoryHTML = categoryHTML + `<ion-item id="newCategoryItem">
+	<ion-button color="vibrant" onclick="addNewCategory('newCategoryId')" slot="end">
+		Añadir
+	  </ion-button>          		
+		<ion-input id="newCategoryId" value="" placeholder="Escriba nombre de la categoría" class="ion-text-wrap">
+		</ion-input>
+	</ion-item>`;
+	listCategories.innerHTML = categoryHTML;	
 };
+
+customElements.define('nav-categories', class NavHome extends HTMLElement {
+	connectedCallback() {
+	  var categoryHTML = "";
+
+	  categoryHTML = `
+		<ion-header translucent>
+		  <ion-toolbar>
+			<ion-title>Categorías</ion-title>
+		  </ion-toolbar>
+		</ion-header>
+		<ion-content fullscreen>
+		  <ion-list id="listCategories"></ion-list>
+		</ion-content>
+	  `;
+	  this.innerHTML = categoryHTML;
+	  console.info(categoryHTML);
+	}
+  });
 
 customElements.define('nav-products', class NavDetail extends HTMLElement {
 	connectedCallback() {
-	  this.innerHTML = `
+	  MAP_PRODUCTS_ID = new Map();
+
+	  var productHTML = "";
+	  productHTML = productHTML +  `
 		<ion-header translucent>
 		  <ion-toolbar>
 			<ion-buttons slot="start">
 			  <ion-back-button defaultHref="/"></ion-back-button>
 			</ion-buttons>
-			<ion-title>`+this.categoryProduct.title+`</ion-title>
+			<ion-title>`+this.categoryProduct.value.title+`</ion-title>
 		  </ion-toolbar>
 		</ion-header>
 		<ion-content fullscreen class="ion-padding">
-		</ion-content>
+		<ion-list id="listProducts">
 	  `;
-	}
+	  this.categoryProduct.value.products.forEach(product=>{
+		indexProduct = indexProduct +1;
+		var idProduct = "product"+indexProduct;
+		MAP_PRODUCTS_ID.set(idProduct, product);
+		productHTML = productHTML + `<ion-item id="`+idProduct+`">
+		<ion-buttons slot="end">
+		  <ion-button color="vibrant" onclick="removeProduct('`+this.categoryProduct.id+`','`+idProduct+`')" >
+				<ion-icon slot="icon-only" name="trash-outline"></ion-icon>
+		  </ion-button>
+		  <ion-button color="vibrant">        		
+			  <ion-icon slot="icon-only"  name="heart-outline"></ion-icon>
+			</ion-button>
+	  </ion-buttons>
+
+		<ion-input value="`+product.title+`" class="ion-text-wrap">
+		</ion-input>
+  </ion-item>`;
+	  });
+	  productHTML = productHTML + `<ion-item id="newProductItem">
+	  <ion-button color="vibrant" onclick="addNewProduct('`+this.categoryProduct.id+`','newProductId')" slot="end">
+		  Añadir
+		</ion-button>          		
+		  <ion-input id="newProductId" value="" placeholder="Escriba nombre del producto" class="ion-text-wrap">
+		  </ion-input>
+	  </ion-item>`;
+	  productHTML = productHTML +`</ion-list></ion-content>`;
+
+	  this.innerHTML = productHTML;
+
+	};
+
   });
 
 function showCategoryDetail(category) {
-	const categoryProduct = MAP_CATEGORIES_ID.get(category);
+	const categoryProduct = {};
+	categoryProduct.id = category;
+	categoryProduct.value = MAP_CATEGORIES_ID.get(category);
 	nav.push('nav-products', {categoryProduct});
 };
 
