1cc6d6b1d3d92b7823f59b74f098bd06a529d24c
David <doliverosescribano82@gmail.com>
2020-05-11 19:36:14 +0200

    no message

--- admin/register.js
+++ admin/register.js
@@ -153,8 +153,19 @@ const fetchRegister = (pUser, pPassword, pMail, pPlan) => {
 	xhr.setRequestHeader('Content-type','application/json');
 	xhr.onload = function () {
 		hideLoading();
-		if (xhr.readyState == 4 && xhr.status == "200") {		
-			confirmRegister();
+		if (xhr.readyState == 4 && xhr.status == "200") {	
+			var response = JSON.parse(xhr.responseText);
+			stripe.redirectToCheckout({
+				// Make the id field from the Checkout Session creation API response
+				// available to this file, so you can provide it as parameter here
+				// instead of the {{CHECKOUT_SESSION_ID}} placeholder.
+				sessionId: response.data.id
+			  }).then(function (result) {
+				  console.info(result);
+				// If `redirectToCheckout` fails due to a browser or network
+				// error, display the localized error message to your customer
+				// using `result.error.message`.
+			  });
 		} else {
 			var response = JSON.parse(xhr.responseText);
 			presentToast(response.message)
